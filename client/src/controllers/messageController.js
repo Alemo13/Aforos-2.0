92b0c0493600f42cf326419db76e3a6e6974a275
Christian Rengifo Mejia <christian2162133@gmail.com>
2022-03-17 02:08:51 -0500

    front commit4

--- client/src/controllers/messageController.js
+++ client/src/controllers/messageController.js
@@ -8,11 +8,7 @@ async function processMessage(message, messageChannel) {
     const messageContent = JSON.parse(message.content.toString());
     // console.log(messageContent);
     const newMessage = new MessageModel({tipo: messageContent.tipo, nombre: messageContent.nombre, capacidad: messageContent.capacidad});
-    if(!newMessage){
-        await newMessage.save();
-    }else{
-        await MessageModel.findByIdAndUpdate(newMessage._id, newMessage)
-    }
+    await newMessage.save();
     messageChannel.ack(message);
 }
 
