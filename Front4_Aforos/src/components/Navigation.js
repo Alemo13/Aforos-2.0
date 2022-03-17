9ed066691f7e93cec0ccff79c9152945f8132ad2
Alemo13 <alejoreme12gmail.com>
2022-03-17 11:03:33 -0500

    last commi

--- Front4_Aforos/src/components/Navigation.js
+++ Front4_Aforos/src/components/Navigation.js
@@ -6,7 +6,7 @@ export default class navigation extends Component {
         return (
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <Link className='navbar-brand' to="/">
-                    <img src="http://drive.google.com/uc?export=view&id=10S_5VMpHDlxx_0f-3rVYxyg1u3AF4id0" className="img-fluid" />
+                    <h4>Aforos</h4>
                 </Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
